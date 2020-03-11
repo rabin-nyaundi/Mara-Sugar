84e5f90ad18ff7a3f1906e3cf52b1e8670199860
Rabinnyd <nyaundirabin@gmail.com>
2020-03-11 10:50:29 +0300

    first commit

---
+++ app/src/main/java/com/rabitech/dataModels/UserDetails.kt
@@ -0,0 +1,10 @@
+package com.rabitech.dataModels
+
+data class user_details
+    (
+    val user_fname: String = "",
+    val user_lname: String = "",
+    val user_phone: String = "",
+    val user_email: String = "",
+    val user_National_id: String = ""
+)
---
+++ app/src/main/java/com/rabitech/dataModels/Users.kt
@@ -0,0 +1,4 @@
+package com.rabitech.dataModels
+
+class Users {
+}
